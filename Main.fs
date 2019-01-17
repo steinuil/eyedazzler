34e4d0473683ccc5895283b325004d2243d7c932
steinuil <steenuil.owl@gmail.com>
2019-01-17 20:26:12 +0100

    use Vector3

--- Main.fs
+++ Main.fs
@@ -2,28 +2,28 @@ open System
 open System.Drawing
 open System.Windows.Forms
 
+open System.Numerics
+
 [<EntryPoint>]
 [<STAThread>]
 let main _args =
     Application.EnableVisualStyles ()
 
-    use form = new Form (Width = 200, Height = 200, Text = "Eyedazzler")
+    use form = new Form (Width = 400, Height = 200, Text = "Eyedazzler")
 
-    let nx, ny = 100, 100
+    let nx, ny = 400, 200
 
     use img = new Bitmap (nx, ny)
 
     form.Paint.Add (fun e ->
         for j = 0 to ny - 1 do
             for i = 0 to nx - 1 do
-                let r = float i / float nx
-                let g = float j / float ny
-                let b = 0.2
+                let vec = Vector3 (float32 i / float32 nx, float32 j / float32 ny, 0.2f)
                 let color = Color.FromArgb
                                 (255,
-                                 int (255.99 * r),
-                                 int (255.99 * g),
-                                 int (255.99 * b))
+                                 int (255.99f * vec.X),
+                                 int (255.99f * vec.Y),
+                                 int (255.99f * vec.Z))
                 img.SetPixel(i, j, color)
 
         e.Graphics.DrawImage (img, 0, 0)
