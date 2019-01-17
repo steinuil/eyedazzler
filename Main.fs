f9e6e2668c6da8b08928176fd85a042c7a1fdc45
steinuil <steenuil.owl@gmail.com>
2019-01-17 17:51:07 +0100

    simple gradient image

--- Main.fs
+++ Main.fs
@@ -8,12 +8,26 @@ let main _args =
     Application.EnableVisualStyles ()
 
     use form = new Form (Width = 200, Height = 200, Text = "Eyedazzler")
-    // use ctx = form.CreateGraphics ()
 
-    // use brush = new SolidBrush (Color.Red)
-    // let rect = new Rectangle (0, 0, 50, 50)
+    let nx, ny = 100, 100
 
-    // ctx.FillRectangle (brush, rect)
+    use img = new Bitmap (nx, ny)
+
+    form.Paint.Add (fun e ->
+        for j = 0 to ny - 1 do
+            for i = 0 to nx - 1 do
+                let r = float i / float nx
+                let g = float j / float ny
+                let b = 0.2
+                let color = Color.FromArgb
+                                (255,
+                                 int (255.99 * r),
+                                 int (255.99 * g),
+                                 int (255.99 * b))
+                img.SetPixel(i, j, color)
+
+        e.Graphics.DrawImage (img, 0, 0)
+    )
 
     Application.Run form
     0
